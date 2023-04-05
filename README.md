673797a1d2e8dfa1047bafae79b925ebf5d23a33
sulabs2021 <129872712+sulabs2021@users.noreply.github.com>
2023-04-05 13:28:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+[![Deploy with Vercel](https://vercel.com/button?utm_source=busiyi&utm_campaign=oss)](https://vercel.com/new/clone?utm_source=busiyi&utm_campaign=oss&repository-url=https://github.com/shenghuaxiong/gpt-4and35-alimtech&env=PASSWORD&env=OPENAI_API_KEY)
---
+++ src/system.ts
@@ -0,0 +1,25 @@
+import type { Model } from "./types"
+
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: "",
+  model: "gpt-3.5-turbo" as Model
+}
+
+export const defaultMessage = `请在设置中输入邀请码（关注公众号alimtech，回复gpt）和APIKEY。自备APIKEY|[网站助手](http://nwzz.xyz)`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
+
+export const defaultMaxInputTokens: Record<Model, number> = {
+  "gpt-3.5-turbo": 3072,
+  "gpt-4": 6144,
+  "gpt-4-32k": 24576
+}
+
+export const defaultModel: Model = "gpt-3.5-turbo"
